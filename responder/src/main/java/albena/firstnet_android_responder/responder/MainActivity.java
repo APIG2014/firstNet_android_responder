68aa73b6816c72bd50d4184b860f0ceff730f972
Albena Iotova <albenaiotova@gmail.com>
2018-10-20 13:30:55 -0500

    project created

---
+++ responder/src/main/java/albena/firstnet_android_responder/responder/MainActivity.java
@@ -0,0 +1,13 @@
+package albena.firstnet_android_responder.responder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
